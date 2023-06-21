1e160e0660fff5f25d9250c105958789876cf5d8
danieljuma5 <jumadaniel275@gmail.com>
2023-06-21 16:41:54 +0300

    Completed assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,17 @@
 class MoviesController < ApplicationController
   wrap_parameters false
   
+  
   def index
     movies = Movie.all
     render json: movies
   end
 
   def create
-    movie = Movie.create(movie_params)
+    movie = Movie.create!(movie_params)
     render json: movie, status: :created
+    rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   private
